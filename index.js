ef5b8d73520ae009a51578843e11fd3eec407ef7
Chris Sefkin <c-sefkin@github.com>
2024-01-04 12:30:42 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const core = require('@actions/core');
 
+try
+
+{
+
+const firstname = core.getInput('firstname');
+
+console.log('Hello ${firstname}!');
+
+}
+
+catch (error)
+
+{
+
+  core.setFailed(error.message);
+
+}
